fbf2f2844e9e62cacf9c2e1530171c641cadaa8c
Priti Chandrabahadur Rana <89412074+PR-Theprogrammer@users.noreply.github.com>
2022-11-14 19:20:30 +0530

    Update CeasarCipher.java

--- CeasarCipher.java
+++ CeasarCipher.java
@@ -3,44 +3,54 @@
 //also, blank spaces are not encrypted
 
 import java.util.*;
+//program for Ceasar Cipher where plaintext are in lower case and cipher in uppercase
 public class CeasarCipher {
-
+	static String letters="abcdefghijklmnopqrstuvwxyz";
 	//encryption function
 	static String encrypt(String pt,int key) {
-		String cipher="";
-		//convert it into uppercase
-		char[] pt_arr=pt.toUpperCase().toCharArray();
-		char indi;
-		for(int i=0;i<pt_arr.length;i++) {
-			if((int)pt_arr[i]==32) {
-				indi=pt_arr[i];
-			}
-			else
-				indi= (char) ((int)pt_arr[i]+key);//for all cases
 
-			cipher+=indi;
+		String cipher="";
+		for (int i = 0; i < pt.length(); i++) {
+			int charPosition = letters.indexOf(pt.charAt(i));
+
+			int keyVal = (charPosition+key ) % 26;
+			if((int)pt.charAt(i)!=32) {
+				cipher += letters.charAt(keyVal);
+			}
+			else {
+
+				cipher += pt.charAt(i);//when white space occurs
+			}
+
 		}
-		return cipher;
+		return cipher.toUpperCase();
 	}
 
 	//decryption function
 	static String  decrypt(String cipher,int key) {
 		String pt="";
-		char[] cipher_arr=cipher.toLowerCase().toCharArray();
-		char indi;
-		for(int i=0;i<cipher_arr.length;i++) {
-			if((int)cipher_arr[i]==32) {
-				indi=cipher_arr[i];
+		cipher=cipher.toLowerCase();
+		for (int i = 0; i < cipher.length(); i++) {
+			int charPosition = letters.indexOf(cipher.charAt(i));
+			int keyVal = (charPosition-key) % 26;
+
+			if(keyVal<0) {
+				keyVal=26+keyVal;
 			}
-			else
-				indi= (char) ((int)cipher_arr[i]-key);
-			pt+=indi;
+
+			if((int)cipher.charAt(i)!=32) {
+				pt += letters.charAt(keyVal);
+			}else {
+
+				pt += cipher.charAt(i);//when white space occurs
+			}
+
 		}
 
 		return pt;
 	}
 	public static void main(String[] args) {
-		
+		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter Plain Text: ");
 		String plain_text=sc.nextLine();
@@ -54,3 +64,4 @@ public class CeasarCipher {
 	}
 
 }
+
